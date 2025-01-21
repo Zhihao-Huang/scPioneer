93bdd4656b92fa6100c3f7b8a3984577eeb465fe
huangzhihao <zhihao.jnu@gmail.com>
2025-01-21 17:31:18 +0800

    v2.1.0

--- data-raw/markerdb_preprocessing.R
+++ data-raw/markerdb_preprocessing.R
@@ -2,53 +2,53 @@
 library(dplyr)
 ## NOT RUN
 if (F) {
-## prepare additional database
-panglaodb <- read.table(sep = '\t', header = T,
-                        file = './data-raw/PanglaoDB_markers_27_Mar_2020.tsv.gz')
-
-# mm10
-orthologs <- biospiper::Get_orthologs_mouse_human(version = 98,
-                                                  remove.duplicated = F,
-                                                  only.one.to.one = F,
-                                                  using.local.file = T)
-orthologs <- orthologs[!duplicated(orthologs$HGNC_symbol) & orthologs$HGNC_symbo != '',]
-panglaodb_mm <- panglaodb[grepl('Mm',panglaodb$species),]
-colnames(panglaodb_mm)[2] <- 'HGNC_symbol'
-panglaodb_mm <- left_join(panglaodb_mm, orthologs)
-
-dim(panglaodb_mm)
-#[1] 6087   17
-sum(is.na(panglaodb_mm$MGI_symbol))
-#[1] 524
-panglaodb_mm <- panglaodb_mm[!is.na(panglaodb_mm$MGI_symbol),]
-# only specific markers; 
-panglaodb_mm <- panglaodb_mm[!duplicated(panglaodb_mm$MGI_symbol),]
-panglaodb_mm <- panglaodb_mm %>% group_by(cell.type) %>% 
-  summarise(geneSymbol = paste(MGI_symbol, collapse = ','))
-colnames(panglaodb_mm)[1] <- 'cellName'
-panglaodb_mm <- as.data.frame(panglaodb_mm)
-rownames(panglaodb_mm) <- panglaodb_mm$cellName
-#write.table(panglaodb_mm, quote = F, sep = '\t', 
-#            file = './data-raw/PanglaoDB_markers_27_Mar_2020_mm.txt')
-
-# hs
-panglaodb_hs <- panglaodb[grepl('Hs',panglaodb$species),]
-colnames(panglaodb_hs)[2] <- 'HGNC_symbol'
-dim(panglaodb_hs)
-#[1] 6034   17
-sum(is.na(panglaodb_hs$HGNC_symbol))
-#[1] 0
-# only specific markers; 
-panglaodb_hs <- panglaodb_hs[!duplicated(panglaodb_hs$HGNC_symbol),]
-panglaodb_hs <- panglaodb_hs %>% group_by(cell.type) %>% 
-  summarise(geneSymbol = paste(HGNC_symbol, collapse = ','))
-colnames(panglaodb_hs)[1] <- 'cellName'
-panglaodb_hs <- as.data.frame(panglaodb_hs)
-rownames(panglaodb_hs) <- panglaodb_hs$cellName
-#write.table(panglaodb_hs, quote = F, sep = '\t', 
-#            file = './data-raw/PanglaoDB_markers_27_Mar_2020_hs.txt')
-
-
+  ## prepare additional database
+  panglaodb <- read.table(sep = '\t', header = T,
+                          file = './data-raw/PanglaoDB_markers_27_Mar_2020.tsv.gz')
+  
+  # mm10
+  orthologs <- biospiper::Get_orthologs_mouse_human(version = 98,
+                                                    remove.duplicated = F,
+                                                    only.one.to.one = F,
+                                                    using.local.file = T)
+  orthologs <- orthologs[!duplicated(orthologs$HGNC_symbol) & orthologs$HGNC_symbo != '',]
+  panglaodb_mm <- panglaodb[grepl('Mm',panglaodb$species),]
+  colnames(panglaodb_mm)[2] <- 'HGNC_symbol'
+  panglaodb_mm <- left_join(panglaodb_mm, orthologs)
+  
+  dim(panglaodb_mm)
+  #[1] 6087   17
+  sum(is.na(panglaodb_mm$MGI_symbol))
+  #[1] 524
+  panglaodb_mm <- panglaodb_mm[!is.na(panglaodb_mm$MGI_symbol),]
+  # only specific markers; 
+  panglaodb_mm <- panglaodb_mm[!duplicated(panglaodb_mm$MGI_symbol),]
+  panglaodb_mm <- panglaodb_mm %>% group_by(cell.type) %>% 
+    summarise(geneSymbol = paste(MGI_symbol, collapse = ','))
+  colnames(panglaodb_mm)[1] <- 'cellName'
+  panglaodb_mm <- as.data.frame(panglaodb_mm)
+  rownames(panglaodb_mm) <- panglaodb_mm$cellName
+  #write.table(panglaodb_mm, quote = F, sep = '\t', 
+  #            file = './data-raw/PanglaoDB_markers_27_Mar_2020_mm.txt')
+  
+  # hs
+  panglaodb_hs <- panglaodb[grepl('Hs',panglaodb$species),]
+  colnames(panglaodb_hs)[2] <- 'HGNC_symbol'
+  dim(panglaodb_hs)
+  #[1] 6034   17
+  sum(is.na(panglaodb_hs$HGNC_symbol))
+  #[1] 0
+  # only specific markers; 
+  panglaodb_hs <- panglaodb_hs[!duplicated(panglaodb_hs$HGNC_symbol),]
+  panglaodb_hs <- panglaodb_hs %>% group_by(cell.type) %>% 
+    summarise(geneSymbol = paste(HGNC_symbol, collapse = ','))
+  colnames(panglaodb_hs)[1] <- 'cellName'
+  panglaodb_hs <- as.data.frame(panglaodb_hs)
+  rownames(panglaodb_hs) <- panglaodb_hs$cellName
+  #write.table(panglaodb_hs, quote = F, sep = '\t', 
+  #            file = './data-raw/PanglaoDB_markers_27_Mar_2020_hs.txt')
+  
+  
   # databases from angrycell
   lazyLoad('/BGFS1/usr/share/packages/R/Rlibs-3.6.1/angrycell/R/sysdata')
   sysdata <- ls()
@@ -71,7 +71,6 @@ rownames(panglaodb_hs) <- panglaodb_hs$cellName
   
 }
 angrycelldata <- readRDS('./data-raw/angrycelldb.rds')
-print(names(angrycelldata))
 cat(paste(paste0(names(angrycelldata), ' <- angrycelldata$', names(angrycelldata)),collapse = '\n'))
 A_M <- angrycelldata$A_M
 addm_qss <- angrycelldata$addm_qss
@@ -143,7 +142,7 @@ usethis::use_data(A_M, addm_qss, D_M, db_self, db_self_mm10, db_user_mm10,
                   mlist2, mlist3, MSigDBlist, MSigDBnames, Rlist, slist, 
                   templv1.2.0, user_links, user_mindrlist1, user_mindrlist2, 
                   user_mlist1, user_mlist2, user_mlist3, user_Rlist, user_slist,
-                  panglaodb_hs,panglaodb_mm, 
+                  panglaodb_hs,panglaodb_mm, orthologslist,symbol_ensembl_list,
                   overwrite = TRUE,
                   internal = TRUE)
 
